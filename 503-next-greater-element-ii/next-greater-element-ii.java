7971700c3cbd9024eb4e1da5a6e2ac4c69a48644
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-10-06 16:32:39 +0530

    Time: 15 ms (67.99%) | Memory: 46.5 MB (39.48%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,13 +1,4 @@
-import java.util.*;
-
 class Solution {
-    public static int findMax(int nums[], int val) {
-        for (int i = 0; i < nums.length; i++) {
-            if (val < nums[i])
-                return nums[i];
-        }
-        return -1;
-    }
 
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> st = new Stack<>();
@@ -19,7 +10,7 @@ class Solution {
                 st.pop();
 
             if (i < nums.length) {
-                arr[idx] = st.isEmpty() ? findMax(nums, nums[idx]) : st.peek();
+                arr[idx] = st.isEmpty() ? -1 : st.peek();
             }
 
             st.push(nums[idx]);
