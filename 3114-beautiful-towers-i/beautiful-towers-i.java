45f9096b377047beb2c9f3c4633b172b854a9970
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-10-09 11:19:20 +0530

    Time: 13 ms (39.19%) | Memory: 44.8 MB (32.43%) - LeetSync

--- 3114-beautiful-towers-i/beautiful-towers-i.java
+++ 3114-beautiful-towers-i/beautiful-towers-i.java
@@ -1,45 +1,22 @@
 class Solution {
-    public static void findLeftMax(long left[], int heights[]) {
-        int n = heights.length;
-        Stack<Integer> st = new Stack<>();
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
-                int top = st.pop();
-                int prev = st.isEmpty() ? -1 : st.peek();
-                sum -= (long)(heights[top] - heights[i]) * (top - prev);
-            }
-            sum += heights[i];
-            left[i] = sum;
-            st.push(i);
-        }
-    }
-
-    public static void findRightMax(long right[], int heights[]) {
-        int n = heights.length;
-        Stack<Integer> st = new Stack<>();
-        long sum = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
-                int top = st.pop();
-                int prev = st.isEmpty() ? n : st.peek();
-                sum -= (long)(heights[top] - heights[i]) * (prev - top);
-            }
-            sum += heights[i];
-            right[i] = sum;
-            st.push(i);
-        }
-    }
-
     public long maximumSumOfHeights(int[] heights) {
-        int n = heights.length;
-        long left[] = new long[n];
-        long right[] = new long[n];
-        findLeftMax(left, heights);
-        findRightMax(right, heights);
         long ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, left[i] + right[i] - heights[i]);
+        for(int peek = 0;peek < heights.length;peek++) {
+            long sum = heights[peek];
+            //left
+            long h = heights[peek];
+            for(int i = peek-1;i >= 0;i--) {
+                h = Math.min(h,(long)heights[i]);
+                sum += h;
+            }
+            //right
+            h = heights[peek];
+            for(int i = peek+1;i < heights.length;i++) {
+                h = Math.min(h,(long)heights[i]);
+                sum += h;
+            }
+
+            ans = Math.max(ans,sum);
         }
         return ans;
     }
