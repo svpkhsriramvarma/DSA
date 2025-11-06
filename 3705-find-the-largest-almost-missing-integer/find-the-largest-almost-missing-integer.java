a0b1cae9153c4128b3a1009e10e54a436ed95dac
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-06 23:50:40 +0530

    Time: 1 ms (99.53%) | Memory: 44.4 MB (37.26%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,25 +1,27 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
+        int[] freq = new int[51];
         int n = nums.length;
-        int[] count = new int[51];
-        for (int x : nums) count[x]++;
+
+        for (int num : nums)
+            freq[num]++;
 
         if (k == 1) {
-            int ans = -1;
-            for (int x : nums) {
-                if (count[x] == 1) ans = Math.max(ans, x);
+            for (int i = 50; i >= 0; i--) {
+                if (freq[i] == 1)
+                    return i;
             }
-            return ans;
+        } else if (k == n) {
+            for (int i = 50; i >= 0; i--) {
+                if (freq[i] > 0)
+                    return i;
+            }
+        } else {
+            int candidate1 = (freq[nums[0]] == 1) ? nums[0] : -1;
+            int candidate2 = (freq[nums[n - 1]] == 1) ? nums[n - 1] : -1;
+            return Math.max(candidate1, candidate2);
         }
 
-        if (k == n) {
-            int ans = -1;
-            for (int x : nums) ans = Math.max(ans, x);
-            return ans;
-        }
-
-        int candidate1 = (count[nums[0]] == 1) ? nums[0] : -1;
-        int candidate2 = (count[nums[n - 1]] == 1) ? nums[n - 1] : -1;
-        return Math.max(candidate1, candidate2);
+        return -1;
     }
 }
