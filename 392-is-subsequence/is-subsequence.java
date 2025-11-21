fdcba79169143cde42c41f83ab611dcf44f5de80
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-21 23:50:13 +0530

    Time: 2 ms (67.34%) | Memory: 42.9 MB (12.19%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ch : s.toCharArray()) {
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        int i = 0;
-        for(char ch : t.toCharArray()) {
-            if(i < s.length() && s.charAt(i) == ch) {
-                map.put(ch,map.get(ch)-1);
+        int i = 0,j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j))
                 i++;
-            }
+            j++;
         }
 
-        for(char ch : s.toCharArray()) {
-            if(map.get(ch) != 0)
-                return false;
-        }
-        return true;
+        return i == s.length();
     }
 }
