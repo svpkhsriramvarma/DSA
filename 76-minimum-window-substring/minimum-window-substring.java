468848c9b206527eb1f784fd4a58c36562cc13ab
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-24 22:56:05 +0530

    Time: 2 ms (99.88%) | Memory: 45.5 MB (45.08%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
         int arr[] = new int[256];
-        int m = s.length(), n = t.length();
-        int minLen = Integer.MAX_VALUE, stIdx = -1;
-        int left = 0, right = 0, count = n;
+        int n = s.length(),m = t.length();
+        int siIdx = -1,minLen = Integer.MAX_VALUE;
+        int right = 0,left = 0;
+        int count = m;
 
-        for (char ch : t.toCharArray()) {
+        for(char ch : t.toCharArray()) {
             arr[ch]++;
         }
 
-        while (right < m) {
+        while(right < n) {
             char ch = s.charAt(right);
-            if (arr[ch] > 0) {
+            if(arr[ch] > 0) {
                 count--;
             }
             arr[ch]--;
 
-            while (count == 0) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    stIdx = left;
+            while(count == 0) {
+                if(right-left+1 < minLen) {
+                    minLen = right-left+1;
+                    siIdx = left;
                 }
-
                 char leftChar = s.charAt(left);
                 arr[leftChar]++;
-                if (arr[leftChar] > 0) {
+                if(arr[leftChar] > 0) {
                     count++;
                 }
                 left++;
-            }
 
+            }
             right++;
         }
 
-        return stIdx == -1 ? "" : s.substring(stIdx, stIdx + minLen);
+        return siIdx == -1 ? "" : s.substring(siIdx,siIdx+minLen);
     }
 }
