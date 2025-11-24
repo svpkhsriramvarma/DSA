988973a31ea0016ed03406ebe81accbc5b8c18a8
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-24 09:13:40 +0530

    Time: 693 ms (10.92%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,14 +1,11 @@
-WITH cte AS (
-    SELECT COUNT(DISTINCT product_key) AS total_products
-    FROM Product
-)
-SELECT 
-    c.customer_id
-FROM 
-    Customer c
-    JOIN cte
-WHERE 1
-GROUP BY 
-    c.customer_id
-HAVING 
-    COUNT(DISTINCT c.product_key) = MAX(cte.total_products);
+# Write your MySQL query statement below
+select
+    customer_id
+from
+    Customer
+group by
+    customer_id
+having
+    count(distinct product_key) = (
+        select count(*) from Product
+    )
