6008b60fda3c2f78d127303ad77c25d67b14fd0c
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-25 08:01:43 +0530

    Time: 2 ms (98.64%) | Memory: 44.3 MB (51.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,31 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        int left = 0,right = s.length()-1;
+    
+    private boolean isAlphaNum(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
 
-        while(left <= right) {
-            if(!Character.isLetterOrDigit(s.charAt(left))) {
+    private char toLower(char c) {
+        if (c >= 'A' && c <= 'Z')
+            return (char)(c + 32);
+        return c;
+    }
+
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left <= right) {
+            while (left < right && !isAlphaNum(s.charAt(left))) {
                 left++;
-                continue;
-            } else if(!Character.isLetterOrDigit(s.charAt(right))) {
+            }
+            while (left < right && !isAlphaNum(s.charAt(right))) {
                 right--;
-                continue;
             }
 
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+            if (toLower(s.charAt(left)) != toLower(s.charAt(right)))
                 return false;
+
             left++;
             right--;
         }
