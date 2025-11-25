a130c83b173c53b4423023a6ed93152ee68f0349
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-25 10:54:44 +0530

    Time: 1 ms (99.77%) | Memory: 46.2 MB (27.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,33 +1,36 @@
 class Solution {
-    public static boolean isSafe(char board[][],int row,int col,char digit) {
-        //veritcal
-        for(int i = 0;i < 9;i++) {
-            if(board[i][col] == digit)
+    public static boolean isSafe(char board[][],char ch,int row,int col) {
+        //horizontal
+        for(int i = 0;i < board.length;i++) {
+            if(board[row][i] == ch)
                 return false;
         }
-        //horisontal
-        for(int i = 0;i < 9;i++) {
-            if(board[row][i] == digit)
+        //vertical
+        for(int i = 0;i < board.length;i++) {
+            if(board[i][col] == ch)
                 return false;
         }
+
         int sr = (row/3)*3;
         int sc = (col/3)*3;
         for(int i = sr;i < sr+3;i++) {
             for(int j = sc;j < sc+3;j++) {
-                if(board[i][j] == digit)
+                if(board[i][j] == ch)
                     return false;
             }
         }
-
         return true;
     }
     public boolean isValidSudoku(char[][] board) {
-        for(int i= 0;i < 9;i++) {
-            for(int j = 0;j < 9;j++) {
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
                 char ch = board[i][j];
                 if(ch != '.') {
                     board[i][j] = '.';
-                    if(!isSafe(board,i,j,ch)) 
+                    if(!isSafe(board,ch,i,j))
                         return false;
                     board[i][j] = ch;
                 }
