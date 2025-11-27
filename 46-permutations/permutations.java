09b4105910222e03d34f464f674dbac023c2a336
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2025-11-27 22:33:43 +0530

    Time: 1 ms (91.21%) | Memory: 45.5 MB (22.78%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,24 @@
 class Solution {
-    public static void solve(List<List<Integer>> res,List<Integer> ans,boolean flag[],int nums[],int size,int idx) {
-        if(ans.size() == size) {
+    public static void solve(int nums[],List<List<Integer>> res,List<Integer> ans,boolean flag[]) {
+        if(ans.size() == nums.length) {
             res.add(new ArrayList<>(ans));
             return;
         }
 
-        for(int i = 0;i < size;i++) {
+        for(int i = 0;i < nums.length;i++) {
             if(flag[i]) continue;
             ans.add(nums[i]);
             flag[i] = true;
-            solve(res,ans,flag,nums,size,idx+1);
+            solve(nums,res,ans,flag);
             ans.remove(ans.size()-1);
             flag[i] = false;
-            
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
         List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         boolean flag[] = new boolean[nums.length];
-        solve(res,ans,flag,nums,nums.length,0);
-        return res;
+        solve(nums,res,ans,flag);
+        return res;   
     }
 }
