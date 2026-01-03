cf7bf0fb15cfaafe7af5efaddb384d26611f8cbe
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2026-01-03 22:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (35.36%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,29 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode mergeList = new ListNode(-1); 
-        ListNode current = mergeList;          
-
-        while(list1 != null && list2 != null) {
-            if(list1.val < list2.val) {
-                current.next = list1;
-                list1 = list1.next;
+        ListNode head1 = list1,head2 = list2;
+        ListNode temp = new ListNode(-1);
+        ListNode prev = temp;
+        while(head1 != null && head2 != null) {
+            if(head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
             } else {
-                current.next = list2;
-                list2 = list2.next;
+                temp.next = head2;
+                head2 = head2.next;
             }
-            current = current.next;
+            temp = temp.next;
         }
-        while(list1 != null) {
-            current.next = list1;
-            current = current.next;
-            list1 = list1.next;
-        } 
-        while(list2 != null) {
-            current.next = list2;
-            current = current.next;
-            list2 = list2.next;
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
         }
-
-        return mergeList.next; 
+        while(head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return prev.next;
     }
 }
